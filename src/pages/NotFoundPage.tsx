5b7757e41cf2e7a4056733178e777c9bc3bc52e7
Axel Munch <92171197+axelmunch@users.noreply.github.com>
2025-11-20 18:45:37 +0100

    Enhanced layout and links

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,12 +1,17 @@
 import { Link, Typography } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
+import { Link as RouterLink } from "react-router";
 
 export default function HomePage() {
   return (
     <>
       <Typography variant="h3">Page not found</Typography>
 
-      <Link href="/" sx={{ display: "flex", alignItems: "center" }}>
+      <Link
+        component={RouterLink}
+        to="/"
+        sx={{ display: "flex", alignItems: "center" }}
+      >
         <HomeIcon fontSize="small" />
         Home
       </Link>
